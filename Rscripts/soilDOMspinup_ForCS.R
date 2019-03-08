5ed6198a0ccff8160f40dc1bfd8664da513764b2
Marco Mina <marcomina@email.com>
2019-03-08 11:29:29 -0500

    Updated inputs. parameters, sim pkgs
    Updated inputs: new biomass-dynamic inputs, updated ForCS inputs with new soil&DOM pools derived from soil spinup, udpated some parameters for RootDynamics, updated content simulation pkgs

--- Rscripts/soilDOMspinup_ForCS.R
+++ Rscripts/soilDOMspinup_ForCS.R
@@ -16,21 +16,23 @@
 library(tidyverse)
 options(scipen=999)
 
-setwd("C:/.../FireWood-LANDIS/inputs_all/")  #working directory. Please adjust accordingly. 
-simpath <- "C:/.../outputs/"                 #path with simulation output folder for the desired landscape (if different from wd)
+setwd("C:/Users/marco/OneDrive - UQAM/Documents/Other Projects/FireWood-LANDIS/inputs_all/")
+simpath <- "E:/LANDIS_ForCS/simulations/MRCCentre/Test01/outputs/Test02_T0-1_soilspinup/" #path with simulation output folder, if different from wd
+
 log_Pools <- read.csv(paste0(simpath,"log_Pools.csv")) ##output file with soil and DOM Pools
 ecocode <- read.table("ecoregion_code_MRCCentre.txt", header = TRUE) #table ecoregion based on order ecor file with their code
 
-log_Pools0 <- log_Pools %>% filter(Time == 0) %>% select(-X) #keep only Year 0
+log_Pools0 <- log_Pools %>% filter(Time == 0) %>% dplyr::select(-X) #keep only Year 0
 log_Pools0_eco <- as.data.frame(log_Pools0 %>%   group_by(ecoregion, species) %>% 
                                   summarise(`1`=mean(VF_A), `2`=mean(VF_B), `3`=mean(Fast_A), `4`=mean(Fast_B), `5`=mean(MED),`6`=mean(Slow_A), 
                                             `7`= mean(Slow_B), `8`= mean(Sng_Stem), `9`= mean(Sng_Oth), `10`= mean(Extra)) %>% 
                                   gather(`1`,`2`,`3`,`4`,`5`,`6`,`7`,`8`,`9`,`10`, key = "DOMpool", value = "Amount") )
 log_Pools0_eco <- merge(log_Pools0_eco, ecocode, by = "ecoregion", all.x = TRUE)
-log_Pools0_eco <- log_Pools0_eco %>% select(ecoregion_sim, species, DOMpool, Amount)
+log_Pools0_eco <- log_Pools0_eco %>% dplyr::select(ecoregion_sim, species, DOMpool, Amount)
 log_Pools0_eco$Amount <- round(log_Pools0_eco$Amount, 4)
-log_Pools0_eco$Amount[log_Pools0_eco$Amount == 0] <- 0.0001 #If no soil spinup is run, values cannot be 0. Thus, replace 0 with 0.0001 (mostly for Pool10)
-# log_Pools0_eco$Amount[is.na(log_Pools0_eco$Amount)] <- 0.0001 #should there be any NA, replace any NA with 0.0001
+log_Pools0_eco$Amount[log_Pools0_eco$Amount == 0] <- 0.0001 #Values cannot be 0. Thus, replace 0 with 0.0001 (mostly for Pool10)
+subset(log_Pools0_eco, is.na(log_Pools0_eco$Amount)) #check for any NAs in Amount
+log_Pools0_eco$Amount[is.na(log_Pools0_eco$Amount)] <- 0.0001 #should there be any NA, replace any NA with 0.0001
 log_Pools0_eco$DOMpool <- as.numeric(log_Pools0_eco$DOMpool)
 
 EcoSppDOMParam <- log_Pools0_eco[order( log_Pools0_eco$ecoregion_sim, log_Pools0_eco$species, log_Pools0_eco$DOMpool ),  ]
@@ -40,8 +42,7 @@ decayrates110 <- c(0.355, 0.5, 0.1435, 0.1435, 0.0374,0.015,0.0033,0.0187,0.0718
 q10values <- c(2.65,	2,	2,	2,	2,	2.65,	1,	2,	2,	2)
 
 #In some ecoregions some species may be absent. Thus, use a loop by ecoregion to paste series of decay rates and q10
-## and add missing species with dummy minimum value 0.001 (otherwise the pool cannot be initialized and model does not start)
-## This issue should not occur if an initial community file with maximum ages is used for DOMsoil spinup process.
+## and add missing species with dummy minimum value 0.001 (otherwise the pool cannot be initialized and the simulation crashes)
 ecoregions <- unique(EcoSppDOMParam$ecoregion_sim)
 speciesall <- unique(EcoSppDOMParam$species)
 EcoSppDOMParam_new <- data.frame()
@@ -50,7 +51,7 @@ for(e in 1:length(ecoregions)) {
   eco <- ecoregions[e]
   aux <- filter(EcoSppDOMParam, ecoregion_sim == eco)
   specieseco <- unique(aux$species) #species present in ecoregion
-  speciesadd <- setdiff(speciesnames,specieseco) #species absent in ecoregion
+  speciesadd <- setdiff(speciesall,specieseco) #species absent in ecoregion
   add <- length(rep(speciesadd, each=10))
   aux_add <- data.frame(ecoregion_sim=rep(eco,add), species=rep(speciesadd, each=10), DOMpool=rep(seq(1,10),add/10), Amount=rep(0.0001,add) )
   aux <- rbind(aux, aux_add)
@@ -60,7 +61,7 @@ for(e in 1:length(ecoregions)) {
 }
 EcoSppDOMParam <- EcoSppDOMParam_new
   
-EcoSppDOMParam <- select(EcoSppDOMParam,ecoregion_sim,species, DOMpool, DecayRate, Amount, Q10)
+EcoSppDOMParam <- dplyr::select(EcoSppDOMParam,ecoregion_sim,species, DOMpool, DecayRate, Amount, Q10)
 colnames(EcoSppDOMParam) <- c("ecoregion","species", "DOMpool", "DecayRate", "AmountT0", "Q10")
 EcoSppDOMParam <- EcoSppDOMParam[order( EcoSppDOMParam$ecoregion, EcoSppDOMParam$species, EcoSppDOMParam$DOMpool ),  ]
 
